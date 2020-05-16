e36c7b766d73de37d246045d5f26de7ab17cf8c2
beatriz.superensino <beatriz.superensino@gmail.com>
2020-05-16 00:09:13 -0200

    Input

---
+++ seção02/aula19.py
@@ -0,0 +1,19 @@
+# input - entrada de dados
+"""
+nome = input('Qual o seu nome? ')
+idade = int(input('Qual é sua idade? '))
+ano_nasc = 2020 - idade
+
+print()
+print(f'O usuário digitou {nome} e o tipo da variável é '
+      f'{type(nome)}, sua idade é {idade}.')
+print(f'Seu ano de nascimento é {ano_nasc}.')
+"""
+###########################################################
+
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+soma = num1 + num2
+
+print()
+print(f'A soma de {num1} + {num2} resulta em: {soma}.')
