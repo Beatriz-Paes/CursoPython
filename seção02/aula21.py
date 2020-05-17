37c78f9672c5722ab628b63f7e36e32bdd8aa63f
beatriz.superensino <beatriz.superensino@gmail.com>
2020-05-16 22:49:13 -0200

    Operadores relacionais

---
+++ seção02/aula21.py
@@ -0,0 +1,20 @@
+"""
+Operadores relacionais + if, elif, else
+==, >, <, <=, >=, !=
+"""
+
+"""num_1 = 2
+num_2 = 5
+
+expressao = num_1 != num_2
+print(expressao)
+"""
+nome = input('Qual o seu nome? ')
+idade = int(input('Qual a sua idade? '))
+
+if idade >= 18:
+    print(f'Olá {nome}, você pode realizar o empréstimo.')
+elif idade == 17:
+    print(f'Olá {nome}, falta pouco para você estar apto a realizar o empréstimo!')
+else:
+    print(f'Olá {nome}, você não pode realizar o empréstimo.')
