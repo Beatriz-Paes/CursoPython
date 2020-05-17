709583d8655105b2847c336da624ba9dceaa32e1
beatriz.superensino <beatriz.superensino@gmail.com>
2020-05-16 22:20:26 -0200

    If, elif, else

---
+++ seção02/aula20.py
@@ -0,0 +1,19 @@
+"""
+IF, ELIF E ELSE + BOOLEANS
+"""
+
+# if (se)
+# else (se não)
+# elif (se não, se)
+
+if False:
+    print('Verdadeiro.')
+elif False:
+    print('Agora é verdadeiro.')
+elif True:
+    print('Mais uma verdadeira.')
+    nome = input('Qual é o seu nome? ')
+    print(f'Seja bem vindo(a) {nome}!')
+else:
+    print('Não é verdadeira.')
+
