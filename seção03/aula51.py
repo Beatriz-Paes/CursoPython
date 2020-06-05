ea62dbbc9d6260b2af3ba2583ce7cfb4d8140fc9
Beatriz-Paes <beah.dev@gmail.com>
2020-06-05 12:54:09 -0400

    Funções - def

---
+++ seção03/aula51.py
@@ -0,0 +1,28 @@
+# Funções - def
+
+######################################################################################
+# variavel = 'valor'
+# print(variavel)
+#
+# def func():
+#     print(variavel)
+#
+# def func2(arg=None):
+#     arg = arg.replace('v', 'c')
+#     return arg
+#
+# func()
+# outra_var = func2(arg=variavel)
+# print(outra_var)
+
+######################################################################################
+# variavel = 'valor'
+# def func():
+#
+#     print(variavel)   # Error
+#     variavel = 1234   # variável referenciada antes de ser criada.
+#     print(variavel)
+#
+# func()
+######################################################################################
+
