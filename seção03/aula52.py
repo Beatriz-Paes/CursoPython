78794f067cad7cb81a6dda029aa81fdbe29019f5
Beatriz-Paes <beah.dev@gmail.com>
2020-06-05 15:51:45 -0400

    Exercícios propostos - Minha solução

---
+++ seção03/aula52.py
@@ -0,0 +1,34 @@
+# Exercícios propostos - Minha solução
+
+"""
+Crie uma funcao1 que recebe uma funcao 2 como parâmetro e retorne o valor da funcao2 executada.
+"""
+
+# def funcao1(funcao2):
+#     return funcao2
+#
+# def funcao2(arg=int(input('Digite um número: '))):
+#
+#     if arg % 2 == 0:
+#         return 'Par'
+#     return 'Ímpar'
+# print(funcao1(funcao2()))
+
+"""
+Crie uma funcao1 que recebe uma funcao2 como parâmetro e retorne o valor da
+funcao2 executada. Faça a funcao1 executar duas funcoes que recebam um número diferente de argumentos.
+"""
+
+# def soma(num1, num2):
+#     return num1 + num2
+#
+# def multip(num3, num4, num5):
+#     return num3 * num4 * num5
+#
+# def funcao1(funcao2):
+#     return funcao2
+#
+# def funcao2():
+#     print(f'O resultado é: {soma(10, 10)}(soma) e {multip(10, 10, 10)}(multiplicação).')
+#
+# funcao1(funcao2())
