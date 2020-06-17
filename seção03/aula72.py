e277d2074164ca5ebb33b91f539065f249ced087
Beatriz-Paes <beah.dev@gmail.com>
2020-06-17 10:17:10 -0400

    Combinations, Permutations e Product

---
+++ seção03/aula72.py
@@ -0,0 +1,22 @@
+"""
+Combinations, Permutations e Product - Itertools
+Combinação - Ordem não importa
+Permutação - Ordem importa
+Ambos não repetem valores únicos
+Produto - Ordem importa e repete valores únicos
+"""
+
+from itertools import combinations, permutations, product
+
+pessoas = ['Luiz', 'Larissa', 'Beah', 'Alder', 'Marcella', 'Ana']
+
+for grupo in combinations(pessoas, 2):
+    print(grupo)
+
+print('##########################################')
+for grupo in permutations(pessoas, 2):
+    print(grupo)
+
+print('##########################################')
+for grupo in product(pessoas, repeat=2):
+    print(grupo)
