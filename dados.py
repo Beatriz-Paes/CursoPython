dedb42f9e054e9bb60290decd73659719420e34e
Beatriz-Paes <beah.dev@gmail.com>
2020-06-18 12:15:24 -0400

    Dados (p/ usar nos códigos)

---
+++ dados.py
@@ -0,0 +1,29 @@
+# Dados para uso posterior durante as aulas
+
+produtos = [
+    {'nome': 'p1', 'preço': 50},
+    {'nome': 'p2', 'preço': 13},
+    {'nome': 'p3', 'preço': 53.69},
+    {'nome': 'p4', 'preço': 39},
+    {'nome': 'p5', 'preço': 5.90},
+    {'nome': 'p6', 'preço': 82},
+    {'nome': 'p7', 'preço': 12},
+    {'nome': 'p8', 'preço': 10},
+    {'nome': 'p9', 'preço': 13},
+    {'nome': 'p10', 'preço': 2.90},
+]
+
+pessoas = [
+    {'nome': 'Beatriz', 'idade': 25},
+    {'nome': 'Ana', 'idade': 25},
+    {'nome': 'Marcella', 'idade': 24},
+    {'nome': 'Larissa', 'idade': 26},
+    {'nome': 'Monique', 'idade': 26},
+    {'nome': 'Wesley', 'idade': 27},
+    {'nome': 'Joyce', 'idade': 27},
+    {'nome': 'Andrew', 'idade': 23},
+    {'nome': 'Brian', 'idade': 28},
+    {'nome': 'Beto', 'idade': 34},
+]
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
