704a7acaa50de4fd11b5ddd3a71e5d44f993ee0f
Beah <beah.dev@gmail.com>
2020-06-29 15:41:26 -0400

    Gerador de CNPJ (a partir do código anterior)

---
+++ seção03/aula90/main.py
@@ -0,0 +1,15 @@
+# Gerador de CNPJ a partir do código anterior
+
+from CursoPython.seção03.aula90 import cnpj
+
+
+cnpj1 = input('Digite um CNPJ: ')
+if cnpj.valida(cnpj1):
+    print(f'{cnpj1} é válido!')
+else:
+    print(f'{cnpj1} é inválido!')
+
+for i in range(50):
+    novo_cnpj = cnpj.gerador()
+    formatado = cnpj.formata(novo_cnpj)
+    print(formatado)
