7349082133a7d54f0e08ac562322b8346714061e
Beah <beah.dev@gmail.com>
2020-06-30 16:02:03 -0400

    @property - Getters e setters

---
+++ seção04/aula96.py
@@ -0,0 +1,40 @@
+# @property - Getters e setters
+
+
+class Produto:
+    def __init__(self, nome, preco):
+        self.nome = nome
+        self.preco = preco
+
+    def desconto(self, percentual):
+        self.preco = self.preco - (self.preco * (percentual / 100))
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, valor):
+        self._nome = valor.title()
+        self._nome = valor.replace('A', '@')
+
+    # Getter
+    @property
+    def preco(self):
+        return self._preco
+
+    # Setter
+    @preco.setter
+    def preco(self, valor):
+        if isinstance(valor, str):
+            valor = float(valor.replace('R$', ''))
+        self._preco = valor
+
+
+p1 = Produto('CAMISA', 50)
+p1.desconto(10)
+print(p1.nome, p1.preco)
+
+p2 = Produto('CANECA', 'R$30')
+p1.desconto(10)
+print(p2.nome, p2.preco)
