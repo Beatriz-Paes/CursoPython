f034493f52051ef180b1eab37d951b034fcbe5b5
Beah <beah.dev@gmail.com>
2020-06-30 14:17:07 -0400

    Classes

---
+++ seção04/aula92/main.py
@@ -0,0 +1,21 @@
+# POO - Classes
+
+from CursoPython.seção04.aula92.pessoas import Pessoa
+
+# p1.comer('maçã')
+# p1.falar('POO')
+# p1.parar_comer()
+# p1.falar('POO')
+# p1.comer('Uva')
+# p1.parar_falar()
+# p1.falar('Python')
+
+p1 = Pessoa('Luiz', 20)
+p2 = Pessoa('Anne', 32)
+
+p1.falar('POO')
+p2.falar('Filmes')  # Objetos independentes
+p1.comer('Legumes')
+
+print(p1.get_ano_nascimento())
+print(p2.get_ano_nascimento())
