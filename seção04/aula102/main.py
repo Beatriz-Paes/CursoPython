424e9f539e72c4072fc4785b9dd1fd040b2b4ae7
Beah <beah.dev@gmail.com>
2020-07-13 14:20:27 -0400

    Herança simples

---
+++ seção04/aula102/main.py
@@ -0,0 +1,14 @@
+# Herança simples
+from seção04.aula102.classes import *
+
+c1 = Cliente('Beatriz', 25)
+c1.falar()
+c1.comprar()
+
+a1 = Aluno('Marcella', 23)
+a1.falar()
+a1.estudar()
+
+
+p1 = Pessoa('Bianca', 24)
+p1.falar()
