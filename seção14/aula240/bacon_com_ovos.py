f6441a91ace50319ef1976968925077867b44743
Beah <beah.dev@gmail.com>
2020-11-21 00:24:47 -0400

    Unittest #1 com TDD

---
+++ seção14/aula240/bacon_com_ovos.py
@@ -0,0 +1,26 @@
+# Unittest #1 com TDD
+"""
+1 - Receber um número inteiro
+2 - Saber se o número NÃO é multiplo de 3 e 5:
+    Passa fome
+3 - Saber se o número é múltiplo somente de 3:
+    bacon
+4 - Saber se o número é múltiplo somente de 5:
+    ovos
+5 - Saber se o número é múltiplo de 3 e 5:
+    bacon com ovos
+
+"""
+
+
+def bacon_com_ovos(n):
+    assert isinstance(n, int), 'n deve ser inteiro.'
+
+    if n % 3 == 0 and n % 5 == 0:
+        return 'Bacon com ovos'
+    if n % 3 == 0:
+        return 'Bacon'
+    if n % 5 == 0:
+        return 'Ovos'
+
+    return 'Passar fome'
---
+++ seção14/aula240/test_bacon_com_ovos.py
@@ -0,0 +1,58 @@
+# Unittest #1 com TDD
+"""
+TDD - Test driver development - (Desenvolvimento dirigido a testes)
+
+Red
+Parte 01 - Criar teste e ver falhar
+
+Green
+Parte 02 - Criar o código e ver o teste passar
+
+Refactor
+Parte 03 - Melhorar meu código
+"""
+
+import unittest
+from seção14.aula240.bacon_com_ovos import bacon_com_ovos
+
+
+class TestBaconComOvos(unittest.TestCase):
+    def test_bacon_com_ovos_deve_levantar_assertionerro_se_n_receber_int(self):
+        with self.assertRaises(AssertionError):
+            bacon_com_ovos('')
+
+    def test_bacon_com_ovos_deve_retornar_bacon_com_ovos_se_entrada_for_multiplo_de_3_e_5(self):
+        entradas = (15, 30, 45, 60)
+        saida = 'Bacon com ovos'
+
+        for entrada in entradas:
+            with self.subTest(entrada=entrada, saida=saida):
+                self.assertEqual(bacon_com_ovos(entrada), saida, msg=f'"{entrada}" não retornou "{saida}".')
+
+    def test_bacon_com_ovos_deve_retornar_passar_fome_se_entrada_não_for_multiplo_de_3_e_5(self):
+        entradas = (1, 2, 4, 7, 8)
+        saida = 'Passar fome'
+
+        for entrada in entradas:
+            with self.subTest(entrada=entrada, saida=saida):
+                self.assertEqual(bacon_com_ovos(entrada), saida, msg=f'"{entrada}" não retornou "{saida}".')
+
+    def test_bacon_com_ovos_deve_retornar_bacon_se_entrada__for_multiplo_somente_de_3(self):
+        entradas = (3, 6, 9, 12, 18, 21)
+        saida = 'Bacon'
+
+        for entrada in entradas:
+            with self.subTest(entrada=entrada, saida=saida):
+                self.assertEqual(bacon_com_ovos(entrada), saida, msg=f'"{entrada}" não retornou "{saida}".')
+
+    def test_bacon_com_ovos_deve_retornar_ovos_se_entrada__for_multiplo_somente_de_5(self):
+        entradas = (5, 10, 20, 25, 35)
+        saida = 'Ovos'
+
+        for entrada in entradas:
+            with self.subTest(entrada=entrada, saida=saida):
+                self.assertEqual(bacon_com_ovos(entrada), saida, msg=f'"{entrada}" não retornou "{saida}".')
+
+
+if __name__ == '__main__':
+    unittest(verbose=True)
